446e7d7801486b3306755c3a71b03c52e53b8cd3
bruno <brunoacy12@gmail.com>
2021-10-25 16:18:56 -0500

    Cuadrado-mobile

--- javas.js
+++ javas.js
@@ -11,3 +11,18 @@ form2.addEventListener(`submit`,(e) =>{
     e.target.reset()
     }
 })
+
+
+const form4 = document.getElementById(`form4`)
+const resultElement4 = document.getElementById(`result4`)
+
+form4.addEventListener(`submit`,(e) =>{
+    e.preventDefault()
+    if(e.target.number8.value != `` && e.target.number8.value > 0){
+    const number8 = parseInt(e.target.number8.value)
+    const result4 =  number8 * number8
+    resultElement4.textContent = `El area del Cuadrado es = ${result4}`
+
+    e.target.reset()
+    }
+})
